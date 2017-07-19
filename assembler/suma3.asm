5fb434f626e421081d2ab7e0f9078f1a46502853
Pedro Muñoz <pmz94@hotmail.com>
2017-07-19 16:33:59 -0700

    Update suma3.asm

--- assembler/suma3.asm
+++ assembler/suma3.asm
@@ -3,19 +3,19 @@
 %include 'funciones.asm'
 
 section .data
-	numero1 		DD 543 	;Numero entero
-	cadena1 		DB "789", 0x0 	;cadena de numeros
-	numero2 		DD 888 	;numero entero
+	numero1 DD 543 		;Numero entero
+	cadena1 DB "789", 0x0 	;cadena de numeros
+	numero2 DD 888 		;numero entero
 
 segment .bss
-	sum resb 4 			;reservamos 4 bytes
+	sum resb 4 	;reservamos 4 bytes
 
 section .text
-	GLOBAL _start 		;punto de entrada
+	GLOBAL _start 	;punto de entrada
 
 _start:
 	mov EAX, cadena1 	;cadena a conversion
-	call atoi 			;convertimos cadenas a enteros
+	call atoi 		;convertimos cadenas a enteros
 	mov [sum], EAX 		;guardamos en suma el numero convertido
 	mov EBX, [numero1] 	;cargamos el numero1 a EBX
 	add EAX, EBX 		;suma EAX y EBX y el resultado en EAX
