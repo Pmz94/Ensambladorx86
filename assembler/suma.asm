a91591ad1dc6dd0219aac772cdeec75bccf71371
Pedro Muñoz <pmz94@hotmail.com>
2017-07-19 16:33:14 -0700

    Update suma.asm

--- assembler/suma.asm
+++ assembler/suma.asm
@@ -4,11 +4,11 @@
 ;1/Mar/17
 
 section .data
-	a DB "5"	;primer operador
-	b DB "4"	;segundo operador
-	msg DB "La Suma Es: ", 0x0 ;mensaje de la
-	len EQU $ - msg		;longitud
-	vac DB " ", 0xA, 0x0 	;renglon vacio
+	a DB "5"			;primer operador
+	b DB "4"			;segundo operador
+	msg DB "La Suma Es: ", 0x0 	;mensaje de la
+	len EQU $ - msg			;longitud
+	vac DB " ", 0xA, 0x0 		;renglon vacio
 	lv EQU $ - vac 			;longitud renglon vacio
 
 segment .bss
@@ -33,14 +33,14 @@ _start:
 	mov EAX, 4 ;systwrite
 	int 0x80 ;llamar al kernel
 
-	mov ECX, sum ;despliega el resultado de la suma
+	mov ECX, sum 	;despliega el resultado de la suma
 	mov EDX, 1
-	mov EBX, 1 ;file descriptor (stdout)
-	mov EAX, 4 ;system call number (sys_write)
-	int 0x80 ;llamar al kernel
+	mov EBX, 1 	;file descriptor (stdout)
+	mov EAX, 4 	;system call number (sys_write)
+	int 0x80 	;llamar al kernel
 
-	mov ECX, vac ;renglon vacio
-	mov EDX, lv ;longitud de renglon vacio
+	mov ECX, vac 	;renglon vacio
+	mov EDX, lv 	;longitud de renglon vacio
 	mov EBX, 1
 	mov EAX, 4
 	int 0x80
