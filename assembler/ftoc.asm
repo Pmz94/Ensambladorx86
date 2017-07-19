bbb0bd59b1db9d5cbe220746b8224b1f75c4c47f
Pedro Muñoz <pmz94@hotmail.com>
2017-07-19 16:22:03 -0700

    Update ftoc.asm

--- assembler/ftoc.asm
+++ assembler/ftoc.asm
@@ -10,7 +10,7 @@ segment .bss
 	num_arg RESB 4
 
 section .text
-	global _start 		;tiene que ser declarado para usar gcc
+	global _start	;tiene que ser declarado para usar gcc
 
 _start:
 	pop ECX 	;capturar e num argumentos en ECX
@@ -18,28 +18,28 @@ _start:
 	jl quit 	;si es asi, cierra el programa
 	pop EAX 	;capturar el nombre del programa
 	dec ECX 	;restar 1 al num de argumentos que tenga ECX
-	mov EBX, 0  ;para inicializar la suma en cero 
-	mov [num_arg], ECX ;
+	mov EBX, 0  	;para inicializar la suma en cero 
+	mov [num_arg], ECX
 
 ciclo:
-	pop EAX 			;obtenemos argumento
-	call atoi 			;convierte el argum a numero
+	pop EAX 		;obtenemos argumento
+	call atoi 		;convierte el argum a numero
 	;call iprintLF
-	call ftoc 			;hace la operacion
+	call ftoc 		;hace la operacion
 	call iprintLF
 	add EBX, EAX 		;sumamos los argumentos
 
-	dec ECX 			;restamos 1 al numero de argumentos
-	cmp ECX, 0 			;checamos si es el ultimo argumento
-	jnz ciclo 			;seguimos si no es el ultimo
-						;si fue el ultimo argumento...
+	dec ECX 		;restamos 1 al numero de argumentos
+	cmp ECX, 0 		;checamos si es el ultimo argumento
+	jnz ciclo 		;seguimos si no es el ultimo
+				;si fue el ultimo argumento...
 	;resultado a imprimir
 	mov EAX, msg 		;mensaje a imprimir
-	call sprint			;llamar funcion para imprimir cadena
-	mov EDX, 0			;multiplicamos acumulado x
+	call sprint		;llamar funcion para imprimir cadena
+	mov EDX, 0		;multiplicamos acumulado x
 	mov EAX, EBX 		;movemos lo que haya en EBX a EAX
 	mov EBX, [num_arg] 	;divisor
 	div EBX
 	call iprintLF 		;imprimimos el argumento
 
-	jmp quit 			;salimos
+	jmp quit 		;salimos
